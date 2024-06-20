1d2c405258d49dc7a6350d3df05172106f44a814
GuilhermePortella <guilhermeportella2@gmail.com>
2024-06-19 23:48:34 -0300

    Iniciando apirestfull

---
+++ src/main/java/br/guilherme/api_restful/API_RESTful.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.guilherme.api_restful;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class API_RESTful {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
