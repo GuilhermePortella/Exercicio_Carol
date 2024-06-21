ba462d2057471cf142b0b9ed9fd56095fd15aaa5
GuilhermePortella <guilhermeportella2@gmail.com>
2024-06-21 20:21:29 -0300

    Finalizando resource

--- src/main/java/br/guilherme/api_restful/PessoaResource.java
+++ src/main/java/br/guilherme/api_restful/PessoaResource.java
@@ -2,6 +2,7 @@ package br.guilherme.api_restful;
 
 import java.util.List;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
@@ -26,29 +27,61 @@ public class PessoaResource {
     public List<Pessoa> get() {
         return _repository.GetAll();
     }
-    
+
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Pessoa getById(@PathParam("id") int id){
+    public Pessoa getById(@PathParam("id") int id) {
         return _repository.Get(id);
     }
-    
+
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response post(Pessoa pessoa){
-        try{
+    public Response post(Pessoa pessoa) {
+        try {
             _repository.Add(pessoa);
             return Response.status(Response.Status.CREATED).entity(pessoa).build();
-        }catch(Exception exception){
+        } catch (Exception exception) {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(exception.getMessage()).build();
         }
     }
-    
+
     @PUT
-    public Response put(){
-        return null;
+    @Path("id")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response put(@PathParam("id") int id, Pessoa pessoa) {
+
+        Pessoa p = _repository.Get(id);
+        if (p == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        try {
+            pessoa.setId(id);
+            _repository.Edit(pessoa);
+            return Response.status(Response.Status.OK).entity(pessoa).build();
+        } catch (Exception exception) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(exception.getMessage()).build();
+
+        }
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response delete(@PathParam("id") int id) {
+        Pessoa pessoa = _repository.Get(id);
+        if (pessoa == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        try {
+            _repository.Delete(id);
+            return Response.status(Response.Status.NOT_FOUND).build();
+        } catch (Exception exception) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(exception.getMessage()).build();
+        }
+
     }
 
 }
